93d18621137f7686612f844613a6883054fc8c4a
Patrick Luan <patrickluan360@gmail.com>
2023-10-15 03:20:47 -0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="dark:bg-indigo-950 dark:text-gray-300 min-h-screen">
+    <RouterView/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
