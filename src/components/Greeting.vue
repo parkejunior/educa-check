0f2b16c1a4580d29e85fb117398dab03349c5aad
Patrick Luan <patrickluan360@gmail.com>
2023-10-15 03:21:17 -0300

    Create components

---
+++ src/components/Greeting.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="text-2xl font-extrabold tracking-tight">Bom dia, Professora!</div>
+</template>
+
+<script setup lang="ts">
+
+</script>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="flex justify-between items-center">
+    <router-link to="/">
+      <ChevronLeftIcon class="w-8 h-8"></ChevronLeftIcon>
+    </router-link>
+
+    <h1><slot name="title"/></h1>
+    <div><slot name="end"/></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ChevronLeftIcon } from '@heroicons/vue/24/outline'
+</script>
